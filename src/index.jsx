9d25cc7833b662b3baeea027994d45a14ad13649
Tahmid Lasker <tahmid@kth.se>
2025-12-03 11:28:55 +0100

    Removed duplicate index.jsx

--- src/index.jsx
+++
@@ -1,4 +0,0 @@
-// TODO make a reactive model (application state), pass it as prop to the components used
-import {createRoot} from "react-dom/client";
-const mountedApp= createRoot(document.getElementById('root'))
-mountedApp.render(<div>hello world!</div>);   
