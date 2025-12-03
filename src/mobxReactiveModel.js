aee08b037de321ff629420fceb4d3dd4ee387611
Jens Lardner <lardner@kth.se>
2025-12-03 11:58:48 +0100

    create model

--- src/mobxReactiveModel.js
+++ src/mobxReactiveModel.js
@@ -1,8 +1,7 @@
 //import "/src/teacherFetch.js"; // protection against fetch() in infinite loops
 import { observable, configure } from "mobx";
 import { reaction } from "mobx";
-//import {model} from "/src/DinnerModel.js";
+import {model} from "/src/studyModel.js";
 configure({ enforceActions: "never", });  // we don't use Mobx actions in the Lab
 
-
-//export const reactiveModel= observable(model);//"make a reactive object out of the model exported from DinnerModel";s
+export const reactiveModel= observable(model);//"make a reactive object out of the model exported from DinnerModel";s
