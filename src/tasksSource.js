52bb3b10ad11ec5510a6249ea40afcb5c22d6845
Jens Lardner <lardner@kth.se>
2025-12-08 09:23:37 +0100

    bug fixes

--- src/tasksSource.js
+++ src/tasksSource.js
@@ -1,36 +1,50 @@
 import { TASKS_URL } from "./apiConfig";
 
+
+function responseACB(response) {
+    if (response.status !== 200)
+        throw new Error("non-200 http response");
+    return response.json();
+}
+
+function returnItemsACB(data) {
+    return data.items || [];
+}
+
+
 export function getAllLists(token) {
 
-    function responseACB(response) {
-        if (response.status !== 200)
-            throw new Error("non-200 http response");
-        return response.json();
-    }
-
-    function returnItemsACB(data) {
-        return data.items || [];
-    }
 
     return fetch(
-         TASKS_URL + "/users/@me/lists",
+        TASKS_URL + "/users/@me/lists",
         {
             headers: {
-                 "Authorization": `Bearer ${token}`
+                "Authorization": `Bearer ${token}`
             }
         }
     ).then(responseACB).then(returnItemsACB);
 }
 
 
-export function getAllTasks() {
-    getAllLists().then()
+export function getAllTasks(token, searchParams) {
 
-    functon getTasksForListACB(list) {
+    function getTaskFromListACB(list) {
         return fetch(
-            TASKS_URL + "/lists/" + list.id + "/tasks",
+            TASKS_URL + "/lists/" + list.id + "/tasks" + "?" + new URLSearchParams(searchParams),
             {
-                headers: {  
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            }
+        ).then(responseACB).then(returnItemsACB);
 
+    }
+
+    function getAllTasksFromListsACB(lists){
+       return lists.map(getTaskFromListACB);
+
+    }
+
+    return getAllLists(token).then(getAllTasksFromListsACB);
 
 }
