ec1a5b3bb2f33b0ddc601bb42e25bad8b96e2bf8
Tahmid Lasker <tahmid@kth.se>
2025-12-17 08:25:55 +0100

    Working on fixing that the sidebar does not show until refresh

--- src/reactjs/ReactRoot.jsx
+++ src/reactjs/ReactRoot.jsx
@@ -1,49 +1,55 @@
 import { observer } from "mobx-react-lite";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider, Outlet } from "react-router-dom";
 import { Overview } from "./overviewPresenter";
 import { Login } from "./loginPresenter";
 import { Timer } from "./timerPresenter";
 import { Sidebar } from "./sidebarPresenter.jsx";
 
 const ReactRoot = observer(function ReactRoot(props) {
-    const location = window.location.hash;
-    const isLoginPage = location === "#/login" || location === "#/";
-    console.log(location);
-    return(
-        <div className="flexParent">
-            <div className="sidebarContent"> 
-                {/* If left is true, return right, else return false */}
-                {!isLoginPage && <Sidebar model={props.model}/>}
-            </div>
-            <div className="mainContent">
-                <RouterProvider router={makeRouter(props.model)} />
-            </div>
-        </div>
-    )
-
     return <RouterProvider router={makeRouter(props.model)} />;
 });
 
-function makeRouter(model){
+function Layout(props) {
+    const location = window.location.hash;
+    const isLoginPage = location === "#/login" || location === "#/";
+    return (
+        <div className="flexParent">
+            <div className="sidebarContent">
+                {/* If left is true, return right, else return false */}
+                {!isLoginPage && <Sidebar model={props.model} />}
+            </div>
+            <div className="mainContent">
+                <Outlet />
+            </div>
+        </div>
+    );
+}
+
+function makeRouter(model) {
     return createHashRouter([
         {
-            path: "/",
-            element: <Login model = {model}/>,
-        },
+            element: <Layout model={model} />,
+            children: [
+                {
+                    path: "/",
+                    element: <Login model={model} />,
+                },
 
-        {
-            path: "/login",
-            element: <Login model = {model}/>,
-        },
+                {
+                    path: "/login",
+                    element: <Login model={model} />,
+                },
 
-        {
-            path: "/overview",
-            element: <Overview model = {model}/>,
+                {
+                    path: "/overview",
+                    element: <Overview model={model} />,
+                },
+                {
+                    path: "/timer",
+                    element: <Timer model={model} />,
+                },
+            ],
         },
-        {
-            path: "/timer",
-            element: <Timer model={model} />,
-        }
     ]);
 }
 
