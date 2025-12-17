3a7876db1909427a32377d4d77f686af94f3ee8d
Tahmid Lasker <tahmid@kth.se>
2025-12-17 08:01:28 +0100

    Sidebar is now displayed on the left side. BUG: Only shows after refreshing the website

--- src/views/sidebarView.jsx
+++ src/views/sidebarView.jsx
@@ -1,55 +1,54 @@
-import { Typography, Button, Stack } from "@mui/material";
+import { Typography, Button, Stack, IconButton } from "@mui/material";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import TimerIcon from "@mui/icons-material/Timer";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LogoutIcon from "@mui/icons-material/Logout";
 
+/**
+ * TODO: PLACEHOLDER. CHANGE LATER.
+ */
 export function SidebarView(props) {
     return (
-        <div style={{
-            width: "250px",
-            backgroundColor: "#1a1d24",
-            height: "100vh",
-            padding: "20px",
-            display: "flex",
-            flexDirection: "column",
-            gap: "20px"
-        }}>
-            <Typography variant="h5" color="white" style={{ marginBottom: "20px" }}>
-                My Study Planner
-            </Typography>
-            
-            <Stack spacing={2}>
-                <Button 
-                    variant="contained" 
-                    fullWidth
-                    onClick={() => window.location.hash = "#/overview"}
-                >
-                    Overview
-                </Button>
-                
-                <Button 
-                    variant="contained" 
-                    fullWidth
-                    onClick={() => window.location.hash = "#/timer"}
-                >
-                    Timer
-                </Button>
-                
-                <Button 
-                    variant="contained" 
-                    fullWidth
-                    onClick={() => window.location.hash = "#/calendar"}
-                >
-                    Calendar
-                </Button>
-                
-                <Button 
-                    variant="outlined" 
-                    color="error"
-                    fullWidth
-                    onClick={props.onLogout}
-                    style={{ marginTop: "auto" }}
-                >
-                    Logout
-                </Button>
+        <Stack
+            sx={{
+                backgroundColor: "#000000ff",
+                height: "100vh",
+                flexDirection: "column",
+            }}
+            justifyContent="space-between"
+        >
+            <Stack>
+                <IconButton>
+                    <AssignmentIcon
+                        onClick={() => {
+                            window.location.hash = "#/overview";
+                        }}
+                        sx={{ color: "white" }}
+                    />
+                </IconButton>
+
+                <IconButton>
+                    <TimerIcon
+                        onClick={() => {
+                            window.location.hash = "#/timer";
+                        }}
+                        sx={{ color: "white" }}
+                    />
+                </IconButton>
+
+                <IconButton>
+                    <CalendarMonthIcon
+                        onClick={() => {
+                            window.location.hash = "#/calendar";
+                        }}
+                        sx={{ color: "white" }}
+                    />
+                </IconButton>
             </Stack>
-        </div>
+
+            <IconButton>
+                <LogoutIcon onClick={props.onLogout} sx={{ color: "white" }} />
+            </IconButton>
+        </Stack>
     );
 }
