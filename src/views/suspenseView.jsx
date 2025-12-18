07c02b405ceca3ffa9dd75bc2533bdab3a90a02e
Tahmid Lasker <tahmid@kth.se>
2025-12-18 09:14:13 +0100

    Added loading circle when there is no data, not sure if this is good

--- src/views/suspenseView.jsx
+++ src/views/suspenseView.jsx
@@ -2,23 +2,28 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export function SuspenseView(props) {
     if (!props.promise) {
-        return <span>no data</span>;
+        // return <span>no data</span>;
+        return loading();
     }
     if (props.promise && props.error) {
         return <span>{props.error.toString()}</span>;
     }
     if (props.promise && !props.data && !props.error) {
-        return (
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                }}
-            >
-                <CircularProgress size="200px" />
-            </div>
-        );
+        return loading();
     }
+}
+
+function loading() {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+            }}
+        >
+            <CircularProgress size="200px" />
+        </div>
+    );
 }
