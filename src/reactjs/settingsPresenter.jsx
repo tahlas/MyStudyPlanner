16d22f26e0f2712cd90d169a6c516afd8b6bcf57
Tahmid Lasker <tahmid@kth.se>
2025-12-20 09:54:13 +0100

    Fixed error when refresing page in settings

--- src/reactjs/settingsPresenter.jsx
+++ src/reactjs/settingsPresenter.jsx
@@ -1,8 +1,12 @@
 import { observer } from "mobx-react-lite";
 import { SettingsView } from "../views/settingsView.jsx";
 import { getCourseNames } from "../utilities.js";
+import { SuspenseView } from "../views/suspenseView.jsx";
 
 const Settings = observer(function SettingsRender(props) {
+    if (!props.model.user) {
+        return <SuspenseView />;
+    }
 
     return (
         <SettingsView user={props.model.user} courses={props.model.courses} />
