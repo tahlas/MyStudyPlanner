cfceb4da6613ee504a296e79a6721cd827b1b31e
Tahmid Lasker <tahmid@kth.se>
2025-12-20 09:09:53 +0100

    Moved the onClick to the IconButton so they are easier to click

--- src/views/sidebarView.jsx
+++ src/views/sidebarView.jsx
@@ -3,12 +3,8 @@ import AssignmentIcon from "@mui/icons-material/Assignment";
 import TimerIcon from "@mui/icons-material/Timer";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import LogoutIcon from "@mui/icons-material/Logout";
-import SettingsIcon from '@mui/icons-material/Settings';
+import SettingsIcon from "@mui/icons-material/Settings";
 
-
-/**
- * TODO: PLACEHOLDER. CHANGE LATER.
- */
 export function SidebarView(props) {
     return (
         <Stack
@@ -20,44 +16,40 @@ export function SidebarView(props) {
             justifyContent="space-between"
         >
             <Stack>
-                <IconButton>
-                    <AssignmentIcon
-                        onClick={() => {
-                            window.location.hash = "#/overview";
-                        }}
-                        sx={{ color: "white" }}
-                    />
+                <IconButton
+                    onClick={() => {
+                        window.location.hash = "#/overview";
+                    }}
+                >
+                    <AssignmentIcon sx={{ color: "white" }} />
                 </IconButton>
 
-                <IconButton>
-                    <TimerIcon
-                        onClick={() => {
-                            window.location.hash = "#/timer";
-                        }}
-                        sx={{ color: "white" }}
-                    />
+                <IconButton
+                    onClick={() => {
+                        window.location.hash = "#/timer";
+                    }}
+                >
+                    <TimerIcon sx={{ color: "white" }} />
                 </IconButton>
 
-                <IconButton>
-                    <CalendarMonthIcon
-                        onClick={() => {
-                            window.location.hash = "#/calendar";
-                        }}
-                        sx={{ color: "white" }}
-                    />
+                <IconButton
+                    onClick={() => {
+                        window.location.hash = "#/calendar";
+                    }}
+                >
+                    <CalendarMonthIcon sx={{ color: "white" }} />
                 </IconButton>
             </Stack>
 
             {/* <IconButton>
                 <LogoutIcon onClick={props.onLogout} sx={{ color: "white" }} />
             </IconButton> */}
-            <IconButton>
-                <SettingsIcon
-                    onClick={() => {
-                        window.location.hash = "#/settings";
-                    }}
-                    sx={{ color: "white" }}
-                />
+            <IconButton
+                onClick={() => {
+                    window.location.hash = "#/settings";
+                }}
+            >
+                <SettingsIcon sx={{ color: "white" }} />
             </IconButton>
         </Stack>
     );
