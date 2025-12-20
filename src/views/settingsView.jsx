7b717626d3a6e8a0b3afa9a0461c75639258df74
Tahmid Lasker <tahmid@kth.se>
2025-12-20 12:39:04 +0100

    Added add course button. does not work yet

--- src/views/settingsView.jsx
+++ src/views/settingsView.jsx
@@ -1,42 +1,81 @@
 import "/src/style.css";
 import { Typography, Divider, Button } from "@mui/material";
 
-
 export function SettingsView(props) {
     return (
-        <div style={{display:"flex", flexDirection:"row"}}>
+        <div style={{ display: "flex", flexDirection: "row" }}>
             {accountInfo(props.user, props.onLogout)}
             {boxWithCourses(props.courses)}
         </div>
     );
 }
 
-function accountInfo(user, onLogout){
-    return(
-        <div className="settingsBoxes">
-            <Typography variant="h4" color="white" paddingLeft="15px" paddingTop="10px" paddingBottom="10px" fontWeight="bold">
-                Account
-            </Typography>
-            <Divider sx={{ backgroundColor: "white" }} variant="middle"/>
-            <img src={user.photoURL} alt="User Profile" style={{ borderRadius: "50%", margin: "15px" }} />
-            <Typography variant="h6" color="white" paddingLeft="15px" paddingBottom="10px">
-                {user.displayName}
-            </Typography>
-            <Typography variant="h6" color="white" paddingLeft="15px" paddingBottom="15px">
-                {user.email}
-            </Typography>
-            <Button variant="outlined" onClick={onLogout} style={{ marginLeft: "15px" }}>Sign Out</Button>
-        </div>
-    );
-
-}
-
-function boxWithCourses(courses){
+function accountInfo(user, onLogout) {
     return (
         <div className="settingsBoxes">
-            <Typography variant="h4" color="white" paddingLeft="15px" paddingTop="10px" paddingBottom="10px" fontWeight="bold">
-                Courses
+            <Typography
+                variant="h4"
+                color="white"
+                paddingLeft="15px"
+                paddingTop="10px"
+                paddingBottom="10px"
+                fontWeight="bold"
+            >
+                Account
             </Typography>
+            <Divider sx={{ backgroundColor: "white" }} variant="middle" />
+            <img
+                src={user.photoURL}
+                alt="User Profile"
+                style={{ borderRadius: "50%", margin: "15px" }}
+            />
+            <Typography
+                variant="h6"
+                color="white"
+                paddingLeft="15px"
+                paddingBottom="10px"
+            >
+                {user.displayName}
+            </Typography>
+            <Typography
+                variant="h6"
+                color="white"
+                paddingLeft="15px"
+                paddingBottom="15px"
+            >
+                {user.email}
+            </Typography>
+            <Button
+                variant="outlined"
+                onClick={onLogout}
+                style={{ marginLeft: "15px" }}
+            >
+                Sign Out
+            </Button>
+        </div>
+    );
+}
+
+function boxWithCourses(courses) {
+    return (
+        <div className="settingsBoxes">
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <Typography
+                    variant="h4"
+                    color="white"
+                    paddingLeft="15px"
+                    paddingTop="10px"
+                    paddingBottom="10px"
+                    fontWeight="bold"
+                >
+                    Courses
+                </Typography>
+                {/* TODO: Add Course on click! */}
+                <Button onClick={() => console.log("TODO")} style={{ marginLeft: "auto", marginRight: "10px" }}>
+                    Add Course
+                </Button>
+            </div>
+
             {courses.map(renderCourseItemCB)}
         </div>
     );
@@ -45,21 +84,36 @@ function boxWithCourses(courses){
 function renderCourseItemCB(course) {
     return (
         <div key={course.name}>
-            <Divider sx={{ backgroundColor: "white" }} variant="middle"/>
-            <div style={{ display: "flex", alignItems: "center", paddingLeft: "15px", paddingTop: "10px", paddingBottom: "10px" }}>
-                <div style={{
-                    width: "20px",
-                    height: "20px",
-                    borderRadius: "50%",
-                    backgroundColor: course.color,
-                    marginRight: "10px"
-                }} />
+            <Divider sx={{ backgroundColor: "white" }} variant="middle" />
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    paddingLeft: "15px",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                }}
+            >
+                <div
+                    style={{
+                        width: "20px",
+                        height: "20px",
+                        borderRadius: "50%",
+                        backgroundColor: course.color,
+                        marginRight: "10px",
+                    }}
+                />
                 <Typography variant="h6" color="white">
                     {course.name}
                 </Typography>
                 {/* TOOD UPDATE COURSE */}
-                <Button onClick={() => console.log("TODO")} sx={{marginLeft: "auto", marginRight: "10px"}}>Edit</Button>
+                <Button
+                    onClick={() => console.log("TODO")}
+                    sx={{ marginLeft: "auto", marginRight: "10px" }}
+                >
+                    Edit
+                </Button>
             </div>
         </div>
-    )
+    );
 }
