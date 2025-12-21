14e26f2eb9f52531f4e0a6f63ab6da9f0c0ef6a6
Jens Lardner <lardner@kth.se>
2025-12-21 17:58:26 +0100

    moved a button

--- src/views/components/scheduleModal.jsx
+++ src/views/components/scheduleModal.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
 import { formatTime } from "../../utilities.js";
-import DeleteEventModal from './DeleteEventModal';
+import DeleteEventModal from './deleteEventModal';
 
 function ScheduleModal({ onClose, tasks, events, onDeleteEvent, deleteOptions}) {
     const [eventToDelete, setEventToDelete] = useState(null);
@@ -39,7 +39,7 @@ function ScheduleModal({ onClose, tasks, events, onDeleteEvent, deleteOptions}) 
                                     style={{ backgroundColor: event.color }}
                                 >
                                     <IconButton
-                                        style={{ position: "absolute", top: 8, right: 8 }}
+                                        style={{ position: "absolute", top: 1, right: 1 }}
                                         size="small"
                                         onClick={(e) => {
                                             e.stopPropagation();
