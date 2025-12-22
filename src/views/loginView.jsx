fb4f6d39329384c42db2833487dd96c229f1f42f
Tahmid Lasker <tahmid@kth.se>
2025-12-22 11:13:34 +0100

    Basic login view

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -2,16 +2,30 @@ import { AppProvider } from "@toolpad/core/AppProvider";
 import { SignInPage } from "@toolpad/core/SignInPage";
 
 export function LoginView(props) {
-
-    function loginACB(){
+    function loginACB() {
         props.login();
     }
-    
-    const providers = [ {id: 'google', name: 'Google'} ];
 
-    return (    
-        <AppProvider>
-            <SignInPage providers={providers} signIn={loginACB} />
-        </AppProvider>
+    const providers = [{ id: "google", name: "Google" }];
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+            }}
+        >
+            <img class="h-48 w-96 object-contain" src="src/images/logo.png" alt="MyStudyPlanner Logo"></img>
+            <div class="text-5xl">Welcome to MyStudyPlanner</div>
+            <span class="text-2xl mt-5">
+                Studying just got{" "}
+                <span class="text-2xl italic font-bold">easier</span>
+            </span>
+            <AppProvider>
+                <SignInPage providers={providers} signIn={loginACB} />
+            </AppProvider>
+        </div>
     );
 }
